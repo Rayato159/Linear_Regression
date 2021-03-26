8f9d75069944dc5887e8047a4312170390c6260e
Rayato159 <Rayato159@gmail.com>
2021-03-26 23:16:52 +0700

    Include KNN

--- K nearest neighbors/Model.c
+++ K nearest neighbors/Model.c
@@ -2,7 +2,33 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main(){
+float pol(float x1, float x2, float y1, float y2){
+    return sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
+}
 
+float sum(float *A, int index){
+    float sum = 0;
+    int i;
+    for(i=0; i<index; i++){
+        sum += A[i];
+    }
+    return sum;
+}
+
+float minimum(float *A){
+    float min = A[0];
+    return min; 
+}
+
+void swap(float *x, float *y){
+    float temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(){
+    float* x = (float *)malloc(6*sizeof(x));
+    float* y = (float *)malloc(6*sizeof(y));
+    char *A[] = {"No", "Fat", "No", "Fat", "Fat", "No"};
     return 0;
 }
