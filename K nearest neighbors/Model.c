d9f0b8fc4cdf6e06bbe8a726447ac4d0ea3eaa16
Rayato159 <Rayato159@gmail.com>
2021-03-27 02:33:38 +0700

    Update KNN

--- K nearest neighbors/Model.c
+++ K nearest neighbors/Model.c
@@ -6,29 +6,38 @@ float pol(float x1, float x2, float y1, float y2){
     return sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
 }
 
-float sum(float *A, int index){
-    float sum = 0;
-    int i;
-    for(i=0; i<index; i++){
-        sum += A[i];
+float minimum(float *min, float *A, int index, int *index_data){
+    *min = A[0];
+    int i, index_r = 0;
+    for(i=1; i<index; i++){
+        if(*min > A[i]){
+            *min = A[i];
+            index_r = i;
+        }
     }
-    return sum;
-}
-
-float minimum(float *A){
-    float min = A[0];
-    return min; 
-}
-
-void swap(float *x, float *y){
-    float temp = *x;
-    *x = *y;
-    *y = temp;
+    *index_data = index_r; 
 }
 
 int main(){
-    float* x = (float *)malloc(6*sizeof(x));
-    float* y = (float *)malloc(6*sizeof(y));
-    char *A[] = {"No", "Fat", "No", "Fat", "Fat", "No"};
+    float data[6][2] = {{40, 150}, {50, 130}, {50,170}, {60,160}, {70,140}, {70,180}};
+    char *A[6] = {"Normal", "Fat", "Normal", "Fat", "Fat", "Normal"};
+    float target[1][2] = {{39, 165}};
+    int size = sizeof(data)/sizeof(data[0]);
+
+    float *r = (float*)malloc(size*sizeof(float)); 
+    int i;
+
+    for(i=0; i<6; i++){
+       r[i] = pol(data[i][0], target[0][0], data[i][1], target[0][1]);
+       printf("data[%d] = %.2f\n", i, r[i]);
+    }
+
+    float min = 0.0;
+    int index_data = 0;
+
+    minimum(&min, r, size, &index_data);
+    printf("\nmin = %.2f @index = %d\n", min, index_data);
+    printf("Result is %s\n", A[index_data]);
+
     return 0;
 }
